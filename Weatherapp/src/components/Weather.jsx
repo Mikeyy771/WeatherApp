eb1a2020944f48ce77b9a45a0a83a6ff1ac097c3
Michael Bondzie <michaelbondzie77@gmail.com>
2025-06-09 11:01:37 +0000

    Adding Assets

--- Weatherapp/src/components/Weather.jsx
+++ Weatherapp/src/components/Weather.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './weather.css';
+import search_icon from '../assets/search.png';
 
 const Weather = () => {
   return (
-    <div>
-      <h1>Weather</h1>
+    <div className='weather'>
+      <div className="search">
+        <input type="text" placeholder='Search' />
+        <img src={search_icon} alt="" />
+      </div>
     </div>
   )
 }
