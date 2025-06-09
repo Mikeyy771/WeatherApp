ef7d93278db6b80d3b0d9b4b1ef2ef33686fa2e1
Michael Bondzie <michaelbondzie77@gmail.com>
2025-06-09 11:25:22 +0000

    styling the search bar

--- Weatherapp/src/components/Weather.jsx
+++ Weatherapp/src/components/Weather.jsx
@@ -5,7 +5,7 @@ import search_icon from '../assets/search.png';
 const Weather = () => {
   return (
     <div className='weather'>
-      <div className="search">
+      <div className="search-bar">
         <input type="text" placeholder='Search' />
         <img src={search_icon} alt="" />
       </div>
